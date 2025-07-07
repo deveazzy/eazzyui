645c563763ca2565aadb8006e26d8fa86f2e9ada
EazZy <deveazzy@gmail.com>
2025-07-07 18:22:34 +0700

    update struktur folder

--- public/config/js-config.php
+++ public/config/js-config.php
@@ -1,5 +1,6 @@
 <script type="module" src="/js/global.js"></script> 
 <script type="module" src="/js/header.js"></script>
 <!-- Vendor -->
- <script src="../assets/vendor//flatpickr/flatpickr.js"></script>
+ <!-- Path diubah menjadi absolut dari root web (folder public) -->
+ <script src="/assets/vendor/flatpickr/flatpickr.js"></script>
  <!-- <script type="module" src="/js/vendor/lucide/lucide.js"></script> -->
