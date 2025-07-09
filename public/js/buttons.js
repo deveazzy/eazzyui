0780c90f2447f5a30d25a1b5ce48353cb2d18d08
EazZy <deveazzy@gmail.com>
2025-07-10 01:31:05 +0700

    fixed buttons

--- public/js/buttons.js
+++ public/js/buttons.js
@@ -1,3 +1,12 @@
+// /**
+//  * js/buttons.js
+//  *
+//  * @author    [EazZy Project]
+//  * @copyright Copyright (c) [2025] [EazZy Project]
+//  * @license   https://opensource.org/licenses/MIT MIT License
+//  * File ini berisi semua tautan ke aset eksternal (CSS, JavaScript)
+//  * dan font yang digunakan di EazZy Project.
+//  */
 document.addEventListener('DOMContentLoaded', () => {
     const initMobileSliderExpand = () => {
         
